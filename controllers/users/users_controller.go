7d28231119cfe77dea0656ec16c0e5b0199bb7cb
Breno Santana <brenosantanabruno@live.com>
2022-10-08 12:51:17 -0300

    Domain Peristence

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 
@@ -37,6 +38,7 @@ func CreateUser(c *gin.Context) {
 		c.JSON(restErr.Status, restErr)
 		return
 	}
+
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
 		c.JSON(saveErr.Status, saveErr)
@@ -45,5 +47,17 @@ func CreateUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, result)
 }
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
--- utils/errors/rest_error.go
+++ utils/errors/rest_error.go
@@ -12,8 +12,16 @@ type RestErr struct {
 
 func NewBadRequestError(message string) *RestErr {
 	return &RestErr{
-		Message: "message",
+		Message: message,
 		Status:  http.StatusBadRequest,
 		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
 	}
 }
